67ef711dad603466a434069a296f8e0203713e71
Raj Kishore Patra <rpatrasm@gmail.com>
2022-10-06 12:09:31 +0530

    Updates

---
+++ precache-manifest.69da1207361145090a5248d0b909ecd7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ae5e1e0a85c7e0412af",
+    "url": "/road_safety/static/css/main.905ab8b9.chunk.css"
+  },
+  {
+    "revision": "1ae5e1e0a85c7e0412af",
+    "url": "/road_safety/static/js/main.1ae5e1e0.chunk.js"
+  },
+  {
+    "revision": "9150df413c5c9260106f",
+    "url": "/road_safety/static/css/1.2a01094d.chunk.css"
+  },
+  {
+    "revision": "9150df413c5c9260106f",
+    "url": "/road_safety/static/js/1.9150df41.chunk.js"
+  },
+  {
+    "revision": "855a44468a7d7c344dfe",
+    "url": "/road_safety/static/js/runtime~main.855a4446.js"
+  },
+  {
+    "revision": "4b9c468adeba890f8a982476b67a9bbd",
+    "url": "/road_safety/static/media/death-sign.4b9c468a.png"
+  },
+  {
+    "revision": "bb3e39bf5becff336124b1d6bdc55246",
+    "url": "/road_safety/static/media/carCrash.bb3e39bf.png"
+  },
+  {
+    "revision": "1be1878b8d77dff85a30775a4537f627",
+    "url": "/road_safety/static/media/india.1be1878b.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/road_safety/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/road_safety/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/road_safety/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/road_safety/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9620408f9e251c8f7052f438a11bfee3",
+    "url": "/road_safety/static/media/videoBg.9620408f.mp4"
+  },
+  {
+    "revision": "55ea7fa11e6e9f2e522d4edb1023134a",
+    "url": "/road_safety/static/media/raahgiri.55ea7fa1.png"
+  },
+  {
+    "revision": "76a39de20a318735895422f46f6d6d6d",
+    "url": "/road_safety/static/media/cycling.76a39de2.gif"
+  },
+  {
+    "revision": "3c19d791023a02ee029292ee1845508a",
+    "url": "/road_safety/index.html"
+  }
+];
